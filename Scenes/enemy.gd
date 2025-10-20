7a780238eb14784e836ac0d2bffb7497b0f44f60
RGH-3 <dsvr513@gmail.com>
2025-10-20 13:22:09 +0300

    Revert "Reapply "world""
    This reverts commit 6520c7132e4b90803f22726b9dd010ed77829450.

--- Scenes/enemy.gd
+++ Scenes/enemy.gd
@@ -23,10 +23,10 @@ func _on_player_detector_body_entered(body: Node2D) -> void:
 	if body is Player:
 		if player == null:
 			player = body
-			print(name + " found the player")
+			
 
 func _on_player_detector_body_exited(body: Node2D) -> void:
 	if body is Player:
 		if player != null:
 			player = null
-			print(name + " lost the player")
+			
